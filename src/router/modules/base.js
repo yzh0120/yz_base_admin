0507ccd3e20ff9074ba06dbf3db6f06a55be4bdb
yzh0120 <644193385@qq.com>
2021-12-27 13:06:53 +0800

    up

--- src/router/modules/base.js
+++ src/router/modules/base.js
@@ -88,5 +88,43 @@ export default {
 			]
 		},
        
+	   {
+		 path: 'css',//修改
+		 name: 'base-css',//修改
+		 component: blank,
+		 meta: {
+		     title: 'css',//修改
+		     icon: 'e627;',
+		 },  
+		 children: [
+		     {
+		         path: 'three',//修改
+		         name: 'base-css-three',//修改
+		         component: () => import(/* webpackChunkName: "css-base" */ "@/views/base/css/three.vue"),//修改
+		         meta: {
+		             title: '三角形',//修改
+		             icon: 'e64f;',
+		         }
+		     },
+		 	{
+		 	    path: '0.5px',//修改
+		 	    name: 'base-css-0.5px',//修改
+		 	    component: () => import(/* webpackChunkName: "css-base" */ "@/views/base/css/0.5px.vue"),//修改
+		 	    meta: {
+		 	        title: '0.5px',//修改
+		 	        icon: 'e64f;',
+		 	    }
+		 	},
+		 	{
+		 	    path: 'filter',//修改
+		 	    name: 'base-css-filter',//修改
+		 	    component: () => import(/* webpackChunkName: "css-base" */ "@/views/base/css/filter.vue"),//修改
+		 	    meta: {
+		 	        title: 'filter',//修改
+		 	        icon: 'e64f;',
+		 	    }
+		 	}
+		 ]
+	   }
     ]
 }
