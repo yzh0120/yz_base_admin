b00ef822d6d148085088286d8487d1d2c08d4829
yzh0120 <644193385@qq.com>
2022-01-05 15:10:25 +0800

    up

---
+++ src/views/element/只读/alert.vue
@@ -0,0 +1,54 @@
+<template>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
+<!-- 
+this.$message.error("") 
+this.$message({
+          message: '恭喜你，这是一条成功消息',
+          type: 'success'
+        });
+		
+ -->
+ 
+ 
+<!-- 	
+let con = await this.$confirm(`确定移除 ？`);
+let con = await this.$confirm("确认该项目申请当期上会吗？", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      })
+
+if (con == "confirm") {
+	//保存修改
+} else if(con=="cancel"){
+   //放弃
+}else{
+	//右上角取消
+}
+
+
+
+
+持续显示
+const copymsg = this.$message({
+            message: "正在复制项目。。。",
+            type: "warning",
+            duration: 0,
+          });
+
+ copymsg.close();
+-->
+
+<!-- 
+ $msgbox(options)                                                                   自定义
+ $alert(message, title, options) 或 $alert(message, options)                          没有取消按钮
+ $confirm(message, title, options) 或 $confirm(message, options)                      有取消按钮
+ $prompt(message, title, options) 或 $prompt(message, options)                        提交内容
+ -->
+ 
