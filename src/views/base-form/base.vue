e7d076e889624a26c57b3288a8eeff0d27234ea7
yzh0120 <644193385@qq.com>
2022-01-07 15:25:34 +0800

    up

--- src/views/base-form/base.vue
+++ src/views/base-form/base.vue
@@ -15,6 +15,7 @@
 				formData:{
 					
 					list:[
+						/*******************************************************输入框*/ 
 						{ type:"input",field:"__input",title:"输入框",rules:[
 							{ required: true, message: "请输入", trigger: "blur" },
 							{ validator: self.$check.demo, trigger: "blur" },
@@ -23,6 +24,7 @@
 						{ type:"input",field:"__input1",title:"输入框",btn:"搜索"},
 
 						{ type:"input",field:"__input11",title:"输入框",append:"元"},
+						/*******************************************************treeselect*/ 
 						{ type:"treeselect",field:"__treeselect",title:"树选择",opt:[], 
 						// props :(node) =>{return { id: node.key,label: node.name,children: node.subOptions,}},
 						// id:'id',label:"label",
@@ -40,10 +42,14 @@
 						  ]
 						},
 						
-
+						/*******************************************************密码框*/ 
 						{type:"password",field:"__password",title:"密码框"},
 
+						/*******************************************************文本域*/ 
+
 						{type:"textarea",field:"__textarea",title:"文本域",row:4},
+
+						/*******************************************************下拉框*/ 
 
 						{type:"select",field:"__select",title:"下拉框",opt:[{text1:"选项一",value1:1},{text1:"选项二",value1:2}],text:"text1",value:"value1"},
 
@@ -53,18 +59,31 @@
 
 						{type:"select",field:"__selectCrate",title:"多选下拉框",opt:[{text:"选项一",value:1},{text:"选项二",value:2}],mult:true},
 
+						
+						/*******************************************************单选框*/ 
+						
 						{type:"radio",field:"__radio",title:"单选框",opt:[{text1:"单选一",value1:1},{text1:"单选二",value1:2}],text:"text1",value:"value1"},
 
+						/*******************************************************多选框*/ 
+						
 						{type:"checkbox",field:"__checkbox",title:"多选框",opt:[{text1:"多选一",value1:1},{text1:"多选二",value1:2}],text:"text1",value:"value1"},
 
+						/*******************************************************日期*/ 
+						
 						{type:"date",field:"__date",title:"日期选择器"},
 
 						{type:"daterange",field:"__daterange",title:"日期段选择器"},
 						
+						/*******************************************************时间*/ 
+						
 						{type:"time",field:"__time",title:"时间选择器"},
 
+						/*******************************************************开关*/ 
+						
 						{type:"switch",field:"__switch",title:"开关",av:1,iav:0,avText:"同意",iavText:"否决"},
 
+						/*******************************************************自动补全*/ 
+						
 						{type:"auto",field:"__auto",title:"自动补全",filter: self.auto_filter,key:"value1"}
 					],
 					data:{__input:"初始值"},
