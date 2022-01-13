8deec4957bd9d8afde9b814c31ef7a3670a4a882
yzh0120 <644193385@qq.com>
2022-01-13 15:52:33 +0800

    up

--- src/tools/fn/base/onlyObj.js
+++ src/tools/fn/base/onlyObj.js
@@ -1,14 +1,24 @@
 //数组对象 去重
 
 export default function(fixedRoutes,selfPath){
-	var hash = {};
-	    fixedRoutes = fixedRoutes.reduce(function (item, next) {
-	      hash[next[selfPath]]
-	        ? ""
-	        : (hash[next[selfPath]] = true && item.push(next));
-	      return item;
-	    }, []);
+	let map = new Map();
+	for (let item of fixedRoutes) {
+	    if (!map.has(item[selfPath])) {
+	        map.set(item[selfPath], item);
+	    }
+	}
+	return [...map.values()];
 }
+
+// export default function(fixedRoutes,selfPath){
+// 	var hash = {};
+// 	    fixedRoutes = fixedRoutes.reduce(function (item, next) {
+// 	      hash[next[selfPath]]
+// 	        ? ""
+// 	        : (hash[next[selfPath]] = true && item.push(next));
+// 	      return item;
+// 	    }, []);
+// }
 
 /* 
 https://www.cnblogs.com/shiyunfront/p/7423977.html
